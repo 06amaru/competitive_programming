9aa6c77a2678cf0af6e2ac0d406fb5a32d075330
jaoks <jeffrey.orihuela@utec.edu.pe>
2021-01-19 00:29:42 -0500

    add commit

---
+++ segment_tree/cses_1735.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+const int N = 2e5+7;
+
+int segment_tree[4*N];
+
+void build(int a[], int vertex, int left, int right) {
+    if (left == right) {
+        segment_tree[v] = a[left];
+    } else {
+        int mid = (left + right)/2;
+        build(a, vertex*2, left, mid);
+        build(a, vertex*2+1, mid+1, right);
+        segment_tree[vertex] = segment_tree[vertex*2] + segment_tree[vertex*2+1];
+    }
+}
+
+int sum(int vertex, int left, int right, int beginq, int endq) {
+    if (beginq > endq) return 0;
+    
+    if (left == beginq && right == endq) {
+        return segment_tree[vertex]    
+    } else {
+        int mid = (left+right)/2;
+        return sum(vertex*2, left, right, 
+    }
+}
+
+int main() {
+    
+}
