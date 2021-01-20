8a0d288a978905ce88e7799c96f90b9dbcae1473
jaoks <jeffrey.orihuela@utec.edu.pe>
2021-01-20 01:56:13 -0500

    sum and build

--- segment_tree/cses_1735.cpp
+++ segment_tree/cses_1735.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+using namespace std;
 
 const int N = 2e5+7;
 
@@ -6,7 +7,7 @@ int segment_tree[4*N];
 
 void build(int a[], int vertex, int left, int right) {
     if (left == right) {
-        segment_tree[v] = a[left];
+        segment_tree[vertex] = a[left];
     } else {
         int mid = (left + right)/2;
         build(a, vertex*2, left, mid);
@@ -17,15 +18,16 @@ void build(int a[], int vertex, int left, int right) {
 
 int sum(int vertex, int left, int right, int beginq, int endq) {
     if (beginq > endq) return 0;
-    
+
     if (left == beginq && right == endq) {
-        return segment_tree[vertex]    
+        return segment_tree[vertex];
     } else {
         int mid = (left+right)/2;
-        return sum(vertex*2, left, right, 
+        return sum(vertex*2, left, mid, beginq, min(endq, mid))
+            + sum(vertex*2+1, mid+1, right, max(beginq, mid+1), endq);
     }
 }
 
 int main() {
-    
+
 }
