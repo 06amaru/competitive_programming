16d07ca15a6f268cf75b08a407a7b7586a94057e
jaoks <jeffrey.orihuela@utec.edu.pe>
2021-04-19 19:55:04 -0500

    working ...

--- backtracking/permutation.cpp
+++ backtracking/permutation.cpp
@@ -1,4 +1,28 @@
-//
-// Created by amaru on 19/04/2021.
-//
+#include <bits/stdc++.h>
 
+using namespace std;
+
+void swap(string &word, int i, int j) {
+    const char temp = word[i];
+    word[i] = word[j];
+    word[j] = temp;
+}
+
+void permutation(string word, int start, int end) {
+    if(start == end) {
+        cout << word << endl;
+    } else {
+        for(int i = start; i < end; i++) {
+            swap(word, start, i);
+            permutation(word, start+1, end);
+            swap(word, i, start);
+        }
+    }
+
+}
+
+int main() {
+    string input;
+    cin >> input;
+    permutation(input, 0, input.size());
+}
